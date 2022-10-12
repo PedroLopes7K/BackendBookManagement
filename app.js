a68a00a3990b70122748f76484e3cd9d0088981d
PedroLopes7K <pedrohenri3601@gmail.com>
2022-10-12 13:19:54 -0300

    backend finished

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
 const express = require('express')
 const app = express()
+const livros = require('./livros')
 
 const port = 3001
 
-app.get('/', (req, res) => {
-  res.send('Olá...Bem vindo Pedro!')
-})
-app.get('/12', (req, res) => {
-  res.send('<h1> Curso de express! </h1>')
-})
-// Exemplode middleware
-const middleware = (req, res, next) => {
-  console.log('tentando acessar a rota transfer...')
-  console.log(`${new Date()}`)
-  next()
-}
-// app.use(middleware);
-
-app.get('/transfer', middleware, (req, res) => {
-  res.send('ACESSANDO ROTA TRANSFER!')
-})
+app.use('/livros', livros)
 
 app.listen(port, () => {
   console.log('Servidor rodando em http://localhost:3001')
--- data/migrations/20221012131313_create_livros.js
+++ data/migrations/20221012131313_create_livros.js
@@ -2,14 +2,21 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+  return knex.schema.createTable('livros', table => {
+    table.increments()
+    table.string('titulo', 80).notNullable()
+    table.string('autor', 80).notNullable()
+    table.string('foto', 100).notNullable()
+    table.integer('ano', 4).notNullable()
+    table.decimal('preco', 9.2).notNullable()
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  return knex.schema.dropTable('livros')
+}
